87eee9e238af2cece355ee92194e0be1043e088f
meetshah1995 <meetshah1995@gmail.com>
2016-08-20 18:56:58 +0530

    Add connection and arg parsing for python bandit agent

--- assig-1/client/bandit-agent.py
+++ assig-1/client/bandit-agent.py
@@ -1,15 +1,43 @@
+import sys
 import socket
 import time
 
-s = socket.socket(socket.AF_INET)
-host = socket.gethostname()
-port = 5001
+error_str = """
+    Usage :
+    python bandit-agent.py
+    [--numArms numArms]
+    [--randomSeed randomSeed]
+    [--horizon horizon]
+    [--explorationHorizon explorationHorizon]
+    [--banditFile banditFile]
+    [--port port]
+    """
 
-s.connect((host, port))
-print s.send(bytes(2))
 
-time.sleep(2)
+def get_arg(arguments):
+    numArms = int(arguments[2])
+    randomSeed = int(arguments[4])
+    horizon = int(arguments[6])
+    explorationHorizon = int(arguments[8])
+    banditFile = str(arguments[10])
+    port = int(arguments[12])
+    return numArms, randomSeed, horizon, explorationHorizon, banditFile, port
 
-print s.send(bytes(2))
-s.close 
+if __name__ == '__main__':
+    n_args = 6
 
+    if len(sys.argv) != n_args * 2 + 1:
+        sys.exit(error_str)
+    else:
+        n, s, h, eh, file_path, port = get_arg(sys.argv)
+
+        s = socket.socket(socket.AF_INET)
+        host = socket.gethostname()
+        port = port
+        s.connect((host, port))
+
+        for i in range(h):
+            print s.send(bytes(2))
+            time.sleep(2)
+
+        s.close
