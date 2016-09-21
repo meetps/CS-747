362dd989aa41614268c1795e32b3289be0841c78
meetshah1995 <meetshah1995@gmail.com>
2016-09-22 00:17:44 +0530

    Add data points

---
+++ assig-2/data/generateMDP.py
@@ -0,0 +1,35 @@
+"""To execute, run:
+python3 generateMDP.py > mdp.txt
+"""
+import random
+
+S = 100
+A = 100
+
+print(S)
+print(A)
+
+R = [[[random.uniform(-1, 1) for s_ in range(S)] for a in range(A)] for s in range(S)]
+
+for s in range(0, S):
+    for a in range(0, A):
+        for sPrime in range(0, S):
+            print(str(R[s][a][sPrime]) + "\t", end="")
+        print()
+
+T = [[[random.uniform(0, 1) for s_ in range(S)] for a in range(A)] for s in range(S)]
+
+# normalize so that probabilities add to one.
+for s in range(0,S):
+	for a in range(0, A):
+		total = sum(T[s][a])
+		T[s][a] = [val / total for val in T[s][a]]
+
+for s in range(0, S):
+    for a in range(0, A):
+        for sPrime in range(0, S):
+            print(str(T[s][a][sPrime]) + "\t", end="")
+        print()
+
+gamma = random.random() # 0 <= gamma  < 1
+print(gamma)
